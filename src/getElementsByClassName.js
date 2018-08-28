75a1d97af85459c03815d89e43f13a286faff0bd
johnpwarren@gmail.com <johnpwarren@gmail.com>
2018-08-27 20:00:09 -0700

    Everything works and passes all the tests. Commited before pomander code cleanup.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,11 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-  var nodes = document.body.children;
 
-console.log(Array.isArray(nodes));
-
+var getElementsByClassName = function(className){
+  var elementsByClass = [];
+  function findElementsByClass(element){
+    if(element.classList !== undefined){
+      if( element.classList.contains(className)){
+        elementsByClass.push(element);
+        }
+      for(var i = 0; i < element.childNodes.length; i++){
+        findElementsByClass(element.childNodes[i]);
+      }
+    }
+  }
+   findElementsByClass(document.body);
+   return elementsByClass;
 };
